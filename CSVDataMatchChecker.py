e26de52cc80174ed6085bdecdf75ef9c3db4c7d3
JL171S <103126092+JL171S@users.noreply.github.com>
2022-06-29 08:53:10 -0500

    Added check for counting missing records

--- CSVDataMatchChecker.py
+++ CSVDataMatchChecker.py
@@ -1,8 +1,9 @@
+import csv
+
 def logInfo(message):   # function for logging info related to the process of the application
-    print(str(message))  # print to console
+    print(str(message))  # logInfo to console
 
 def CSVComparison(firstCSVPath : str, firstCSVKey : str, secondCSVPath : str, secondCSVKey : str): # Read two CSV files and compare two keys for matches
-    import csv
     foundMatches = []
     itterationsCount = 0
     with open(firstCSVPath, encoding='utf-8') as CSV1:      # read CSV file
@@ -19,22 +20,41 @@ def CSVComparison(firstCSVPath : str, firstCSVKey : str, secondCSVPath : str, se
                         # logInfo("found match")                          # print
                         break
                     else:
-                        # logInfo(str(firstRow[firstCSVKey])  + " | " + str(secondRow[secondCSVKey]))     # Debug log
                         if (str(secondRow[secondCSVKey]) == '' or str(firstRow[firstCSVKey]) == ''):
+                            # logInfo(str(firstRow[firstCSVKey])  + " | " + str(secondRow[secondCSVKey]))     # Debug log if empty string
                             break
                     itterationsCount += 1
     
         logInfo("Matches found: " + str(len(foundMatches)))
-        logInfo("records itterated over: " + str(itterationsCount))
+        logInfo("records itterated over: " + str(itterationsCount))        
+        # for match in foundMatches:
+        #     logInfo(match)
+        return foundMatches
  
 def main(): 
-    firstCSVToCompare = ""               # CSV filename.csv
-    firstCSVFieldToCompareFrom = ''      # CSV header key
+    firstCSVToCompare = ""                  # CSV filename.csv
+    firstCSVFieldToCompareFrom = ''         # CSV header key
 
-    secondCSVToCompare = ""              # CSV filename.csv
-    secondSVFieldToCompareFrom = ''      # CSV header key
+    secondCSVToCompare = ""                 # CSV filename.csv
+    secondCSVieldToCompareFrom = ''         # CSV header key
 
-    CSVComparison(firstCSVToCompare, firstCSVFieldToCompareFrom, secondCSVToCompare, secondSVFieldToCompareFrom)
+
+    resultsFound = CSVComparison(secondCSVToCompare, secondCSVieldToCompareFrom, firstCSVToCompare, firstCSVFieldToCompareFrom)
+    
+    unfoundMatches = []
+
+    with open(firstCSVToCompare, encoding='utf-8') as CSV1:         # read CSV file
+        firstCSVReader = csv.DictReader(CSV1)                       # load csv file data using csv library's dictionary reader
+
+        for firstRow in firstCSVReader:                             # itterate through first CSV rows
+            found = False
+            for match in resultsFound:
+                if (str(firstRow[firstCSVFieldToCompareFrom]) == match):
+                    found = True
+                    break
+            if found == False:
+                unfoundMatches.append(str(firstRow[firstCSVFieldToCompareFrom]))
+        logInfo("Total records not found: "  + len(unfoundMatches))
 
 
 if __name__ == "__main__":
